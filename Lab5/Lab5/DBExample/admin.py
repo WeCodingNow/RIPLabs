6a472d7d6e6e66e8a50c306e79f1c374aefb0ae6
nigraforce <aleshka2012@gmail.com>
2018-12-24 10:59:36 +0300

    добавил сущности в админку

--- Lab5/Lab5/DBExample/admin.py
+++ Lab5/Lab5/DBExample/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Programmer, Program
+
+
+class ProgrammerProgramsInline(admin.TabularInline):
+    model = Program.programmers.through
+
+
+class ProgrammerAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email', 'telephone']
+    search_fields = ['username', 'id']
+    inlines = [
+        ProgrammerProgramsInline,
+    ]
+
+class ProgramAdmin(admin.ModelAdmin):
+    inlines = [
+        ProgrammerProgramsInline,
+    ]
+    exclude = ('programmers',)
 
 # Register your models here.
+
+
+admin.site.register(Programmer, ProgrammerAdmin)
+admin.site.register(Program, ProgramAdmin)
